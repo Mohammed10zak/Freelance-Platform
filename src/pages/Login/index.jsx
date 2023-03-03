b8c26968fa68ebeb90d4d376e0a182fa246cc6d1
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-03 18:51:37 +0300

    adding error boundary component

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+
+import * as yup from "yup";
+import axios from "axios";
+import { API_URL } from "./../../config/api";
+
+import google from "../../assets/google.svg";
+import apple from "../../assets/apple.png";
+import passwordshow from "../../assets/eye.png";
+
+import { useAuthContext } from "../../context/AuthContext";
+
+import {
+  AlredyAccount,
+  ErrorMessage,
+  InputWrapper,
+  Register,
+  ShowPassword,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledLink,
+  StyledOR,
+  SubmitButton,
+} from "./style";
+
+const regularExpression =
+  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+function Login() {
+  const { loading, setLoading, errors, setErrors, setToken, setAuthorized } =
+    useAuthContext();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordType, setPasswordType] = useState("password");
+
+  const handlePasswordShow = (e) => {
+    e.preventDefault();
+    setPasswordType(passwordType === "password" ? "text" : "password");
+  };
+
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Invalid email")
+      .required("User Name is required"),
+
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .matches(regularExpression, "Invalid Password")
+      .required("Password is required"),
+  });
+
+  const handleChangeInput = (e) => {
+    const { value, id } = e.target;
+    if (id === "username") setEmail(value);
+    if (id === "password") setPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    schema
+      .validate(
+        {
+          email,
+          password,
+        },
+        { abortEarly: false }
+      )
+      .then(async () => {
+        const res = await axios.post(`${API_URL}/users/login`, {
+          email: email,
+          password: password,
+        });
+        if (res) {
+          setToken(res.data.token);
+          localStorage.setItem("token", res.data.token);
+
+          setToken(res.data.token);
+          setErrors([]);
+          setLoading(false);
+          setAuthorized(true);
+        }
+      })
+      .catch((e) => {
+        const errors = e.inner.reduce((acc, error) => {
+          acc[error.path] = error.message;
+          return acc;
+        }, {});
+        setErrors(errors);
+        setLoading();
+      });
+  };
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <Register>Log in to Freelance Platform</Register>
+
+      <InputWrapper>
+        <label htmlFor="username">Username or Email</label>
+        <input
+          type="email"
+          id="username"
+          onChange={handleChangeInput}
+          value={email}
+          placeholder="Email or phone"
+        />
+      </InputWrapper>
+      {errors && errors.email && <ErrorMessage>{errors.email}</ErrorMessage>}
+      <InputWrapper>
+        <label htmlFor="password">Password</label>
+        <input
+          type={passwordType}
+          id="password"
+          onChange={handleChangeInput}
+          value={password}
+          placeholder="Type Here"
+        />
+
+        {errors && errors.password && (
+          <ErrorMessage>{errors.password}</ErrorMessage>
+        )}
+        <ShowPassword
+          src={passwordshow}
+          alt="passwordshow"
+          className="passwordshow"
+          onClick={handlePasswordShow}
+        />
+      </InputWrapper>
+
+      <SubmitButton type="submit">
+        {loading ? "loading..." : "  Log In"}
+      </SubmitButton>
+
+      <StyledOR>
+        <div></div> <span>or</span> <div></div>
+      </StyledOR>
+
+      <StyledButton facebook={apple}>
+        <StyledImg src={google} alt="google" />
+        Continue with Google
+      </StyledButton>
+
+      <StyledButton>
+        <StyledImg src={apple} alt="facebook" facebook={apple} />
+        Continue with apple
+      </StyledButton>
+
+      <AlredyAccount>Don’t have an Freelance account?</AlredyAccount>
+      <StyledLink to="/signup"> Sign Up now</StyledLink>
+    </StyledForm>
+  );
+}
+
+export default Login;
