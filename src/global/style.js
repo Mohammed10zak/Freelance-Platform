3449241ee6d25258997b34d50970c401a5e20207
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-04 14:23:21 +0300

    create global style

---
+++ src/global/style.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+
+body {
+    font-family: 'Arial';
+}
+
+ul {
+    list-style: none;
+}
+
+
+.active {
+  text-decoration: none;
+  color: black;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+}
+
+
+`;
