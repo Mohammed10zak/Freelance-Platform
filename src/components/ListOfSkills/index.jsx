b7d7efc2336382df60b4c183b69b548be4c34bd9
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-08 12:37:24 +0300

    create List of skills component & styled it

---
+++ src/components/ListOfSkills/index.jsx
@@ -0,0 +1,13 @@
+import { Styledskill } from "./style";
+
+const Technology = ({ technologiesOfItem }) => {
+  return (
+    <>
+      {technologiesOfItem?.map((techItem, index) => (
+        <Styledskill key={index}>{techItem}</Styledskill>
+      ))}
+    </>
+  );
+};
+
+export default Technology;
---
+++ src/components/ListOfSkills/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Styledskill =styled.p`
+display: inline-block;
+background-color:#EEEEEE;
+border-radius: 10px;
+padding: 5px 10px;
+margin:6px 2px;
+color: #999;
+
+`
