8c2e5a570df93b794f5991a08f0dc42cc74d0f29
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-14 19:47:05 +0300

    modify jobCard and style it

--- src/components/JobCard/style.js
+++ src/components/JobCard/style.js
@@ -1,29 +1,55 @@
 import styled from "styled-components";
 
 export const Cardjob = styled.div`
-  padding: 20px 30px;
-  margin-bottom: 50px;
-
+  position: relative;
+  padding: 20px 50px;
   &:hover {
-    background-color: #f5f5f5;
+    background-color: #f2f7f2;
+    border-radius: 6px;
     & h3 {
       color: #3c8224;
     }
   }
-`;
 
-  export const JobTitle = styled.h3`
-  margin-top: 50px;
-  `;
+
+`;
 
 export const Jobdetail = styled.div`
   font-size: 14px;
   display: flex;
   color: #888;
   gap: 2px;
+
+  & span {
+  color: #888;
+
+  }
 `;
 
 export const JobDescription = styled.p`
-  line-height: 22px;
-  letter-spacing: 2px;
+`;
+
+export const ToggleSaveBtn = styled.button`
+  position: absolute;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  top: 40px;
+  right: 30px;
+`;
+
+
+export const Jobdetail2=styled.div`
+display:flex;
+gap:10px;
+& span {
+  color: #888;
+
+  }
+`
+export const SeeMore = styled.span`
+  display: inline;
+  color: #108a00;
+  cursor: pointer;
+
 `;
