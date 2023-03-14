f8e77d71b094c0f025e1f21678528ccd1a10dfb5
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-14 20:05:28 +0300

    adding style for recentSearch value component

---
+++ src/pages/Home/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const RecentSearch = styled.div`
+  display: flex;
+  margin: 10px;
+  & span {
+    color: #108a00;
+    margin-right: 10px;
+    &:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  }
+`;
