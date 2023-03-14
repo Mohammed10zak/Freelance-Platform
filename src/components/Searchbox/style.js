4d8b01f32d3be934d7630ca859f212dd4e7634ae
mohammed zakout <mohammedzakkh@gmail.com>
2023-03-14 19:58:01 +0300

    modify style for search icon

--- src/components/Searchbox/style.js
+++ src/components/Searchbox/style.js
@@ -3,11 +3,24 @@ import styled from "styled-components";
 export const Search = styled.form`
   display: flex;
   margin-top: 10px;
+  align-items: center;
 `;
 
-export const Iconsearch = styled.div`
-  width: 45px;
+// export const Iconsearch = styled.div`
+//   width: 45px;
+//   background-color: #3c8224;
+//   border-radius: 6px;
+//   margin-top: 5px;
+// `;
+
+export const Iconsearch = styled.button`
+  width: 35px;
   background-color: #3c8224;
-  border-radius: 6px;
-  margin-top: 5px;
+  height: 38px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  margin-left: 17px;
 `;
